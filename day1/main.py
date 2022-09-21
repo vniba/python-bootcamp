dd4627f942237ad61ace9d7181ef53ec6495e2b2
vniba <abinv321@gmail.com>
2022-09-21 16:19:15 +0530

    day1: string | variable | print | input

---
+++ day1/main.py
@@ -0,0 +1,22 @@
+# print('Hello World! \nHello World')
+# print('Hello' + ' Doe')
+# print(len(input('what is your name? ')))
+# variables  😍
+# name = input('what is your name? ')ś
+# print(name)
+# name = 'jon doe'
+# print(name)
+
+# inp = input('what is ur name? \n')
+# length = len(inp)
+# print(length)
+#
+# username = 'jon'
+# user_age = 88
+# ismarried = 'no'
+
+print('welcome to the band generator')
+user_place = input('whre u grew up \n')
+user_pet = input('whats ur pet name \n')
+print('your band name could be ' + user_pet + ' ' + user_place)
+
